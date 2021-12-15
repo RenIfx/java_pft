a8ed2c12ebcf8b3efc74f5c3379bd398743c016d
RenIfx <ifxminsk@gmail.com>
2021-12-15 19:39:36 +0300

    дз выполнено

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,43 +3,51 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    hello("world");
-    hello("user");
-    hello("Renat");
+    // hello("world");
+    // hello("user");
+    // hello("Renat");
 
-    Square s = new Square(5);
+//    Square s = new Square(5);
 
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    //  System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4, 6);
+    // Rectangle r = new Rectangle(4, 6);
 
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-
-
+    // System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
 
+// расчет растояния между 2 точками коордианат
 
-
+    int n = 0;
     Point p1 = new Point(2, 3);
-    System.out.println("Координаты точки p1 = (" + p1.x + "," + p1.y + ")");
+    //System.out.println("Координаты точки p1 = (" + p1.x + "," + p1.y + ")");
 
     Point p2 = new Point(5, 7);
-    System.out.println("Координаты точки p2 = (" + p2.x + "," + p2.y + ")");
+    //System.out.println("Координаты точки p2 = (" + p2.x + "," + p2.y + ")");
 
-
-
-    double px = (p1.x- p2.x);
-    double py = (p1.y- p2.y);
-    double d = Math.sqrt(px*px+py*py);
-    System.out.println("D = корень/(x1-x2)^2+(y1-y2)^2/" + " D = корень ( " + px*px + " + " + py*py + ") =" + d);
-    System.out.println("Расстояние между точками p1 и p2 = (" + p2.x + "," + p2.y + ")");
-    System.out.println(d);
+    System.out.println("Формула расчета расстояния между точками на плоскости: Растояние = корень/(x1-x2)^2+(y1-y2)^2/");
+    System.out.println("Расстояние между точками на плоскости = корень ( " + distance(p1, p2)*distance(p1, p2) + " + " + p1 + ") =" + distance(p1, p2));
+    System.out.println("Расстояние между точками p1 и p2 = (" + p1 + "," + p2 + ")");
+    System.out.println(p2.distance(p1));
+    System.out.println(p1.distance(p2));
+   // System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
+  public static double distance(Point p1, Point p2) {
+    double px = (p1.x - p2.x);
+  //  System.out.println("dx^2= " + px*px);
+    double py = (p1.y - p2.y);
+  //  System.out.println("dy^2= " + py*py);
+
+  //  System.out.println("D^2= " + (px * px + py * py));
+
+    return Math.sqrt(px * px + py * py);
+
+  }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
+  }
 
-}
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,16 +2,35 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  public int x;
-  public int y;
+  public double x;
+  public double y;
+  static int n;
 
-  public Point (int x, int y) {
+
+
+
+  public Point (double x, double y) {
     this.x = x;
     this.y = y;
+    n++;
+    System.out.println("Координаты точки p" +n +" = (" + x + "," + y + ")");
 
   }
-  public double length (){
-    return Math.sqrt(this.x*this.x-this.y*this.y);
+  public double distance(Point p2){
+    return Math.sqrt((this.x-p2.x)*(this.x-p2.x)+(this.y-p2.y)*(this.y-p2.y));
 
   }
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  @Override
+  public String toString() {
+    return "Point{" + "x=" + x + ", y=" + y + '}';
+  }
+
 }
